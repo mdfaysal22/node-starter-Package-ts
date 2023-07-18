4874c40cd2da762b5a037abebc3c3ef72a31f468
mdfaysal22 <mdfaysal.freelancer@gmail.com>
2023-07-18 13:30:48 +0600

    Husky Check

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const app: Application = express()
 
 // Body parser and Core Setup
 app.use(cors())
-app.use(express.json())
+app.use(express.json(.))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
