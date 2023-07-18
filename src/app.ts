c192bce1e33cdcc297839bab282959aa0b932832
mdfaysal22 <mdfaysal.freelancer@gmail.com>
2023-07-18 12:06:05 +0600

    Add

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,16 @@
-import express, { Application } from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-const app : Application = express();
-
-
+import express, { Application } from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+const app: Application = express()
 
 // Body parser and Core Setup
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-
-
-app.get("/", async(req, res) => {
-    res.send("Hello World");
+app.get('/', async (req, res) => {
+  res.send('Hello World')
 })
 
-export default app;
+export default app
--- src/app/utils/database.ts
+++ src/app/utils/database.ts
@@ -1,17 +1,16 @@
-import mongoose from "mongoose";
-import config from "../../config";
+import mongoose from 'mongoose'
+import config from '../../config'
 const database = async () => {
-
-    if(!config.databaseUrl){
-        console.log("Database URL not found")
-    }else{
-        try{
-            await mongoose.connect(config.databaseUrl as string);
-            console.log('Database Connected')
-        }catch(error){
-            console.log("Database Connected Failed : ", error);
-        }
+  if (!config.databaseUrl) {
+    console.log('Database URL not found')
+  } else {
+    try {
+      await mongoose.connect(config.databaseUrl as string)
+      console.log('Database Connected')
+    } catch (error) {
+      console.log('Database Connected Failed : ', error)
     }
-};
+  }
+}
 
-export default database;
+export default database
